16ba8a1767cc5490feb9f10dfd301076cbec5296
Aarav Varshney <aaravvarshney22@gmail.com>
2022-02-09 18:02:06 +0530

    add small changes

--- src/intellectuals/urls.py
+++ src/intellectuals/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='index.html'), name='logout'),
     path('login/', views.login, name='login'),
     path('receive', views.receive)
 ]
