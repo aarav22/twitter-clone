7c55b9785571b119592a64615e87ea816b28dd14
Aarav Varshney <aaravvarshney22@gmail.com>
2022-02-17 12:43:25 +0530

    migrate to psql

--- src/twitter/settings.py
+++ src/twitter/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'twitter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'hero',
+        'PASSWORD': 'my_db@123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 INTERNAL_IPS = [
